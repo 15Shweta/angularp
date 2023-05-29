ffcff980bd1bccae8b6527bce7097906525ca9f4
15Shweta <pawarshweta2002@gmail.com>
2023-05-29 15:37:21 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:SignupComponent,
+    path:"",
+  },
+  {
+    component:LoginComponent,
+    path:"login",
+  },
+  {
+    component:HomeComponent,
+    path:'home',
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
